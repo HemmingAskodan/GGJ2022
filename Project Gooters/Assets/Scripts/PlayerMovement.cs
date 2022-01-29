f88a5ee38b0a795a41227646241fae753c21bf81
HemmingP <hemming_lol@hotmail.com>
2022-01-29 00:26:14 +0000

    Goose update
    A goose update :+1:

--- Project Gooters/Assets/Scripts/PlayerMovement.cs
+++ Project Gooters/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerMovement : MonoBehaviour
 {
 
     public float movementSpeed = 5f;
 
     Rigidbody2D rb2D => GetComponent<Rigidbody2D>();
+    SpriteRenderer spriteRenderer => GetComponent<SpriteRenderer>();
     private float xVelocity = 0;
+    private float yVelocity = 0;
+    private bool spriteFlipped = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +33,24 @@ public class PlayerMovement : MonoBehaviour
         rb2D.velocity = new Vector2(xVelocity * movementSpeed, rb2D.velocity.y);
     }
 
+    void flipImage()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+        spriteFlipped = !spriteFlipped;
+    }
+
     private void OnMove(InputValue value){
-        xVelocity = value.Get<Vector2>().x;
+        Vector2 v = value.Get<Vector2>();
+        xVelocity = v.x;
+        yVelocity = v.y;
+
+        if(v.x > 0f && !spriteFlipped)
+        {
+            flipImage();
+        }
+        if(v.x < 0f && spriteFlipped)
+        {
+            flipImage();
+        }
     }
 }
